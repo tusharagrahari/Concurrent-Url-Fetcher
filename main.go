fe3f5e44d7100fb3077b3e48c434aed59990348f
Tushar Agrahari <agrahari.tushar84@gmail.com>
2026-05-24 02:12:21 +0530

    Add context for time-out logic

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -18,6 +20,8 @@ type Result struct {
 func main() {
 	argsWithoutProg := os.Args[1:]
 
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
 	if len(argsWithoutProg) == 0 {
 		fmt.Println("Please provide URLs e.g: https://google.com")
 		return
@@ -28,20 +32,35 @@ func main() {
 
 	for _, v := range argsWithoutProg {
 		wg.Add(1)
-		go makeConnection(v, &wg)
+		go makeConnection(v, &wg, ctx)
 	}
 
 	wg.Wait()
 }
 
-func makeConnection(url string, wg *sync.WaitGroup) {
+func makeConnection(url string, wg *sync.WaitGroup, ctx context.Context) {
 	defer wg.Done()
 	start := time.Now()
-	resp, err := http.Get(url)
+	// resp, err := http.Get(url)
+	req,err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
+		fmt.Println("Malformed url")
+	}
+	resp, err := http.DefaultClient.Do(req)
+
+	if errors.Is(err, context.DeadlineExceeded) {
 		res := Result{
 			url:    url,
-			status: "Failed",
+			status: "Connection Timed out",
+			code:   500,
+			time:   time.Since(start),
+		}
+		fmt.Printf("url: %v, result: %v, statusCode: %v, (%v)\n", res.url, res.status, res.code, res.time)
+		return
+	} else if err != nil {
+		res := Result{
+			url:    url,
+			status: err.Error(),
 			code:   500,
 			time:   time.Since(start),
 		}
